42c57ded698813007df3190d0127f923e3c036ad
ULIVZ <472590061@qq.com>
2018-07-29 15:47:44 +0800

    refactor: simplify implementation

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 module.exports = function (rawPromise, defaultValue, timeout) {
-  return new Promise((resolve, reject) => {
-    const timer = setTimeout(() => resolve(defaultValue), timeout)
-    rawPromise
-      .then(output => {
-        clearTimeout(timer)
-        resolve(output)
-      })
-      .catch(err => {
-        clearTimeout(timer)
-        reject(err)
-      })
-  })
+  return Promise.race([
+    rawPromise,
+    new Promise(resolve => setTimeout(() => resolve(defaultValue), timeout))
+  ])
 }
